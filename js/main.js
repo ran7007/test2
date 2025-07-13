1977340d28cf881393a59cd70d605fb6bda97600
ran7007 <korok7007@gmail.com>
2025-07-14 00:56:57 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// $(document).ready(function () {
+
+//     $('.nav').hover(
+//         function () {
+//             // do nothing on enter
+//         },
+//         function () {
+//             // 마우스가 nav_wrap 영역 벗어나면 닫기
+//             $('.sub_gnb').stop(true, true).slideUp(200);
+//         }
+//     );
+
+//     $('.gnb li').hover(function () {
+//         var index = $(this).data('index');
+
+//         $('.sub_gnb').stop(true, true).slideDown(200);
+//         $('.sub_gnb .depth2').hide().eq(index).show();
+//     });
+// });
